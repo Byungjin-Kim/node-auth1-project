ab2b6420fb8f562146970f51fa5d7239c160a705
Byungjin-Kim <composerjins@gmail.com>
2023-05-25 00:20:44 -0500

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -11,9 +11,6 @@ const {
   checkUsernameExists
 } = require("./auth-middleware")
 
-
-
-
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
 
@@ -46,7 +43,6 @@ router.post('/register', checkPasswordLength, checkUsernameFree, (req, res, next
     .catch(next);
 });
 
-
 /**
   2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
 
@@ -64,8 +60,16 @@ router.post('/register', checkPasswordLength, checkUsernameFree, (req, res, next
  */
 
 router.post('/login', checkUsernameExists, (req, res, next) => {
-  res.json('login');
-})
+  const { password } = req.body;
+  if (bcrypt.compareSync(password, req.user.password)) {
+    req.session.user = req.user;
+    res.json({
+      message: `Welcome ${req.user.username}!`
+    });
+  } else {
+    next({ status: 401, message: 'Invalid credentials' });
+  }
+});
 
 
 
